cf3dca669b428476348cd896a0b6a84fa11a7c0b
김유진 <yoojin027@mteletec.com>
2025-06-18 12:48:52 +0900

    feat: [Express] 차단된 확장자 추가 API 구현

--- extension-filter-express/src/routes.ts
+++ extension-filter-express/src/routes.ts
@@ -1,9 +1,15 @@
 import GetFilteredExtensionsAction from "./controller/GetFilteredExtensionsAction";
+import PostFilteredExtensionsAction from "./controller/PostFilteredExtensionsAction";
 
 export const AppRoutes = [
     {
         path: "/api/filtered-extensions",
         method: "get",
         action: GetFilteredExtensionsAction
+    },
+    {
+        path: "/api/filtered-extensions",
+        method: "post",
+        action: PostFilteredExtensionsAction
     },
 ]
