8383de3be5b1899a690451951b5285305a024b38
김유진 <yoojin027@mteletec.com>
2025-06-18 12:17:31 +0900

    chore: [Express] 테스트 API 제거

--- extension-filter-express/src/routes.ts
+++ extension-filter-express/src/routes.ts
@@ -1,15 +1,2 @@
-import GetTestAction from "./controller/GetTestAction";
-import GetFilteredExtensionsAction from "./controller/GetFilteredExtensionsAction";
-
 export const AppRoutes = [
-    {
-        path: "/api/test",
-        method: "get",
-        action: GetTestAction
-    },
-    {
-        path: "/api/filtered-extensions",
-        method: "get",
-        action: GetFilteredExtensionsAction
-    },
 ]
